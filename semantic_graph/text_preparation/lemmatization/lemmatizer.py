071ec470575aa40179235e813b33667c39f58551
kirillmiltsev <milceffk@mail.ru>
2025-03-26 02:40:45 +0700

    refactor: change paths and naming

--- semantic_graph/text_preparation/lemmatization/lemmatizer.py
+++ semantic_graph/text_preparation/lemmatization/lemmatizer.py
@@ -5,11 +5,11 @@ class Lemmatizer:
     def __init__(
         self, text: str, lemmatization_strategy: ILemmatizationStrategy
     ) -> None:
-        self.__text: str = text
+        self.__raw_text: str = text
         self.__lemmatization_strategy = lemmatization_strategy
 
     def make_lemmatization(self) -> list[str]:
-        return self.__lemmatization_strategy.make_lemmatization(self.__text)
+        return self.__lemmatization_strategy.make_lemmatization(self.__raw_text)
 
     def change_strategy(self, lemmatization_strategy: ILemmatizationStrategy) -> None:
         self.__lemmatization_strategy = lemmatization_strategy
