34c202297b0e87c099de7c04d443e576f471433d
kirillmiltsev <milceffk@mail.ru>
2025-03-28 01:32:49 +0700

    refactor: move file content

--- semantic_graph/semantic_graph.py
+++ semantic_graph/semantic_graph.py
@@ -1,2 +1,29 @@
+from dataclasses import dataclass, field
+
+
+@dataclass(frozen=True)
+class Term:
+    title: str
+    description: str
+    title_lemmas: list[str]
+    description_lemmas: list[str]
+
+    def __hash__(self) -> int:
+        return frozenset(self.title_lemmas).__hash__()
+
+
+@dataclass
+class Node:
+    term: Term
+    links: list["Link"] = field(default_factory=list)
+    weight: float = 0.0
+
+
+@dataclass
+class Link:
+    next_node: "Node"
+    weight: float = 0.0
+
+
 class SemanticGraph:
     pass
