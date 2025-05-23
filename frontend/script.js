18590eb07a8f34079aaa35ed545e73a498ccfd5f
kirillmiltsev <milceffk@mail.ru>
2025-05-23 21:36:12 +0700

    change line to arrow

--- frontend/script.js
+++ frontend/script.js
@@ -2,7 +2,7 @@
 const graph = new graphology.Graph();
 graph.addNode("1", { label: "Node 1", x: 0, y: 0, size: 10, color: "blue" });
 graph.addNode("2", { label: "Node 2", x: 1, y: 1, size: 20, color: "red" });
-graph.addEdge("1", "2", { size: 5, color: "purple", label: "Edge from 1 to 2" });
+graph.addEdge("1", "2", { type: 'arrow', size: 5, color: "purple", label: "Edge from 1 to 2" });
 
 // Instantiate sigma.js and render the graph
 const sigmaInstance = new Sigma(graph, document.getElementById("container"), {
