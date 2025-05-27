a433085688ab94d060fea44a8d3ab9c0c4754ae5
kirillmiltsev <milceffk@mail.ru>
2025-05-27 19:59:05 +0700

    change orm-query on raw-sql-query

--- semantic_graph/routes.py
+++ semantic_graph/routes.py
@@ -2,13 +2,13 @@ import json
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, UploadFile, HTTPException, BackgroundTasks
-from sqlalchemy import select
+from sqlalchemy import select, text, column
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_session
 from source_extraction.models import TermModel
 from schemas import GraphLink
-from models import ScientificCatalogModel, ScientificCatalogProcessingStatus
+from models import ScientificCatalogModel, ScientificCatalogProcessingStatus, SemanticGraphLink
 from background_tasks import generate_graph_task
 
 DBSessionDepends = Annotated[AsyncSession, Depends(get_session)]
@@ -17,20 +17,41 @@ router = APIRouter()
 
 @router.get("/scientific-catalog-graph/{catalog_name}")
 async def get_scientific_catalog(catalog_name: str, session: DBSessionDepends) -> list[GraphLink]:
-    stmt = select(ScientificCatalogModel).where(ScientificCatalogModel.title == catalog_name)
+    stmt = select(ScientificCatalogModel.id).where(ScientificCatalogModel.title == catalog_name)
     result = await session.execute(stmt)
-    catalog_entry: ScientificCatalogModel = result.unique().scalars().one_or_none()
+    catalog_id: int | None = result.unique().scalars().one_or_none()
 
-    if catalog_entry is None:
+    if catalog_id is None:
         raise HTTPException(status_code=404, detail=f"Scientific catalog with title '{catalog_name}' not found")
+
+    stmt = text("""
+    SELECT 
+      terms_from.title as term_from_title,
+      terms_from.hash as term_from_hash,
+      terms_to.title as term_to_title,
+      terms_to.hash as term_to_hash,
+      links.weight as weight
+    FROM public.semantic_graph_links links
+    LEFT JOIN public.terms terms_from
+     ON terms_from.id = links.term_from_id
+    LEFT JOIN public.terms terms_to
+     ON terms_to.id = links.term_to_id
+    WHERE links.catalog_id = :catalog_id
+    """)
+    data = {"catalog_id": catalog_id}
+    result = await session.execute(stmt, data)
+
+    column_names = result.keys()
+    rows_data = result.fetchall()
+    links = [dict(zip(column_names, row_data)) for row_data in rows_data]
 
     response = [
         GraphLink(**dict(
-            term_from=dict(title=graph_link.term_from.title, hash=graph_link.term_from.term_hash),
-            term_to=dict(title=graph_link.term_to.title, hash=graph_link.term_to.term_hash),
-            weight=graph_link.weight,
+            term_from=dict(title=graph_link["term_from_title"], hash=graph_link["term_from_hash"]),
+            term_to=dict(title=graph_link["term_to_title"], hash=graph_link["term_to_hash"]),
+            weight=graph_link["weight"],
         ))
-        for graph_link in catalog_entry.graph_links
+        for graph_link in links
     ]
     return response
 
